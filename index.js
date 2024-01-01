ea36d4fb69577f2ba46d867d1985af773c04ae8d
taitai2107 <nguyencongkhanhtai21072001@gmail.com>
2024-01-01 23:55:45 +0700

    Initial commit

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const configCors = require("./src/config/CORS");
 require("express-async-errors");
 const useRouter = require("./src/router/index");
 require("dotenv").config();
-
+console.log("check 1");
 const PORT = process.env.PORT || 5000;
 // app.use("/", (req, res) => {
 //   return res.send("Middleware applied to every request");
 // });
 configCors(app);
-const testConnect = require("./src/config/connectsq");
-testConnect();
+// const testConnect = require("./src/config/connectsq");
+// testConnect();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 useRouter(app);
 app.use("/", (req, res) => {
   res.send("OK");
 });
-// app.use((req, res) => {
-//   res.json("router not found");
-// });
+app.use((req, res) => {
+  res.json("router not found");
+});
 app.listen(PORT, () => {
   console.log(`sever is running on PORT ${PORT}`);
 });
--- src/models/index.js
+++ src/models/index.js
@@ -5,21 +5,21 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = /*process.env.NODE_ENV*/ "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+//}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
@@ -43,7 +43,7 @@ Object.keys(db).forEach((modelName) => {
     db[modelName].associate(db);
   }
 });
-
+console.log("check config :", config, "end");
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
