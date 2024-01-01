043ba5cae7017e7c8632c4dfe462f5ebcf67f120
taitai2107 <nguyencongkhanhtai21072001@gmail.com>
2024-01-01 23:04:04 +0700

    Initial commit

--- src/config/connectsq.js
+++ src/config/connectsq.js
@@ -1,14 +1,15 @@
-// const { Sequelize } = require("sequelize");
-// const sequelize = new Sequelize("nodejs_basic", "root", null, {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
-// const testConnect = async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Connection has been established successfully.");
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// };
-// module.exports = testConnect;
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize("nodejs_basic", "root", null, {
+  host: "127.0.0.1",
+  dialect: "mysql",
+  port: 3306,
+});
+const testConnect = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+module.exports = testConnect;
